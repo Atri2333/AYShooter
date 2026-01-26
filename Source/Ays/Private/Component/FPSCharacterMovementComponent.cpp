a0f417345ee10984cc97f375a41a146507fc2e80
Atri2333 <AtriKawaii@outlook.com>
2026-01-26 19:53:43 +0800

    Update: 客户端预测的Sprint

--- Source/Ays/Private/Component/FPSCharacterMovementComponent.cpp
+++ Source/Ays/Private/Component/FPSCharacterMovementComponent.cpp
@@ -11,6 +11,116 @@
 #include "Player/AysPlayerController.h"
 
 
+UFPSCharacterMovementComponent::FSavedMove_FPS::FSavedMove_FPS()
+{
+	Saved_bWantsToSprint = false;
+}
+
+bool UFPSCharacterMovementComponent::FSavedMove_FPS::CanCombineWith(const FSavedMovePtr& NewMove,
+                                                                    ACharacter* InCharacter, float MaxDelta) const
+{
+	const FSavedMove_FPS* NewFPSMove = static_cast<const FSavedMove_FPS*>(NewMove.Get());
+	if (Saved_bWantsToSprint != NewFPSMove->Saved_bWantsToSprint)
+	{
+		// Sprint状态不同，不能合并
+		return false;
+	}
+	
+	return FSavedMove_Character::CanCombineWith(NewMove, InCharacter, MaxDelta);
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::Clear()
+{
+	FSavedMove_Character::Clear();
+
+	Saved_bWantsToSprint = false;
+}
+
+uint8 UFPSCharacterMovementComponent::FSavedMove_FPS::GetCompressedFlags() const
+{
+	uint8 Result = FSavedMove_Character::GetCompressedFlags();
+	if (Saved_bWantsToSprint)
+	{
+		// Custom flag 0 represents sprinting
+		Result |= FLAG_Custom_0;
+	}
+	return Result;
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::SetMoveFor(ACharacter* Character, float InDeltaTime,
+	FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData)
+{
+	FSavedMove_Character::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	if (IsValid(Character))
+	{
+		if (UFPSCharacterMovementComponent* FPSCMC = Cast<UFPSCharacterMovementComponent>(Character->GetCharacterMovement()))
+		{
+			Saved_bWantsToSprint = FPSCMC->bWantsToSprint;
+		}
+	}
+}
+
+void UFPSCharacterMovementComponent::FSavedMove_FPS::PrepMoveFor(ACharacter* Character)
+{
+	FSavedMove_Character::PrepMoveFor(Character);
+
+	if (IsValid(Character))
+	{
+		if (UFPSCharacterMovementComponent* FPSCMC = Cast<UFPSCharacterMovementComponent>(Character->GetCharacterMovement()))
+		{
+			FPSCMC->bWantsToSprint = Saved_bWantsToSprint;
+		}
+	}
+}
+
+UFPSCharacterMovementComponent::FNetworkPredictionData_Client_FPS::FNetworkPredictionData_Client_FPS(
+	const UCharacterMovementComponent& ClientMovement) : FNetworkPredictionData_Client_Character(ClientMovement)
+{
+}
+
+FSavedMovePtr UFPSCharacterMovementComponent::FNetworkPredictionData_Client_FPS::AllocateNewMove()
+{
+	return FSavedMovePtr(new FSavedMove_FPS());
+}
+
+void UFPSCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	bWantsToSprint = (Flags & FSavedMove_FPS::FLAG_Custom_0) != 0;
+}
+
+void UFPSCharacterMovementComponent::OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation,
+	const FVector& OldVelocity)
+{
+	Super::OnMovementUpdated(DeltaSeconds, OldLocation, OldVelocity);
+
+	if (MovementMode == MOVE_Walking)
+	{
+		if (bWantsToSprint)
+		{
+			MaxWalkSpeed = Sprint_MaxWalkSpeed;
+		}
+		else
+		{
+			MaxWalkSpeed = Walk_MaxWalkSpeed;
+		}
+	}
+}
+
+FNetworkPredictionData_Client* UFPSCharacterMovementComponent::GetPredictionData_Client() const
+{
+	// UE内部也用了const_cast
+	if (ClientPredictionData == nullptr)
+	{
+		UFPSCharacterMovementComponent* MutableThis = const_cast<UFPSCharacterMovementComponent*>(this);
+		MutableThis->ClientPredictionData = new FNetworkPredictionData_Client_FPS(*this);
+	}
+
+	return ClientPredictionData;
+}
+
 void UFPSCharacterMovementComponent::InitLocomotionComponent()
 {
 	AAysPlayer* Player = Cast<AAysPlayer>(CharacterOwner);
@@ -28,7 +138,7 @@ void UFPSCharacterMovementComponent::InitLocomotionComponent()
 
 void UFPSCharacterMovementComponent::InitBasicLocomotion()
 {
-	MaxWalkSpeed = WalkSpeed;
+	MaxWalkSpeed = Walk_MaxWalkSpeed;
 	if (LocomotionStateComponent != nullptr)
 		LocomotionStateComponent->OnLocomotionStateChanged.AddUObject(this, &ThisClass::HandleStateChange);
 }
@@ -71,11 +181,12 @@ void UFPSCharacterMovementComponent::HandleStateChange(const FGameplayTag& Tag, 
 	{
 		if (bAdded)
 		{
-			MaxWalkSpeed = RunSpeed;
+			// 会通过SetMoveFor同步到SavedMove里传给Server
+			bWantsToSprint = true;
 		}
 		else
 		{
-			MaxWalkSpeed = WalkSpeed;
+			bWantsToSprint = false;
 		}
 	}
 	else if (Tag == Tags.State_Locomotion_Crouch)
--- Source/Ays/Public/Component/FPSCharacterMovementComponent.h
+++ Source/Ays/Public/Component/FPSCharacterMovementComponent.h
@@ -16,7 +16,43 @@ class AYS_API UFPSCharacterMovementComponent : public UCharacterMovementComponen
 {
 	GENERATED_BODY()
 	
+	class FSavedMove_FPS : public FSavedMove_Character
+	{
+		uint8 Saved_bWantsToSprint:1;
+	public:
+		FSavedMove_FPS();
+
+		// 检测当前移动和新移动是否可以合并
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* InCharacter, float MaxDelta) const override;
+		// 重置SavedMove
+		virtual void Clear() override;
+		// 将本次移动的关键布尔状态压缩到位标志位
+		virtual uint8 GetCompressedFlags() const override;
+		// 将本次移动的状态保存到SavedMove中
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, FNetworkPredictionData_Client_Character& ClientData) override;
+		// 和SetMoveFor相反，将SavedMove中的状态应用到Character上，主要在重放和纠正状态应用
+		virtual void PrepMoveFor(ACharacter* Character) override;
+	};
+
+	class FNetworkPredictionData_Client_FPS : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FNetworkPredictionData_Client_FPS(const UCharacterMovementComponent& ClientMovement);
+
+		// 分配我们自定义的FSavedMove_FPS
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
+	bool bWantsToSprint = false;
+	
 public:
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+
+	virtual void OnMovementUpdated(float DeltaSeconds, const FVector& OldLocation, const FVector& OldVelocity) override;
+
+	// 获取我们自己的NetworkPredictionData_Client
+	virtual FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+	
 	void InitLocomotionComponent();
 	void InitBasicLocomotion();
 	virtual void BeginPlay() override;
@@ -26,10 +62,10 @@ public:
 	void HandleStateChange(const FGameplayTag& Tag, bool bAdded);
 
 	UPROPERTY(EditAnywhere)
-	float WalkSpeed = 300.f;
+	float Sprint_MaxWalkSpeed = 750.f;
 
 	UPROPERTY(EditAnywhere)
-	float RunSpeed = 600.f;
+	float Walk_MaxWalkSpeed = 300.f;
 
 	UPROPERTY(EditAnywhere, Category = "Deprecared")
 	float CrouchAlpha = 0.f;
